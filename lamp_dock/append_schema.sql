78933f8505b434fadd8cce713a6c0d7b51fab424
yusa.ymmt <yusa.ymmt2903@hotmail.com>
2020-04-18 11:50:06 +0900

    テーブル作成しなおしました

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,18 +1,24 @@
-<<<<<<< HEAD
 CREATE TABLE history(
 history_id INT AUTO_INCREMENT,
 user_id INT,
-item_id INT,
-amount INT,
 total INT,
 created DATETIME,
 updated DATETIME,
-=======
-CREATE TABLE ec_history(
-history_id INT AUTO_INCREMENT,
-user_id INT,
-item_id INT
-create_datetime DATETIME,
->>>>>>> develop
 primary key(history_id)
 );
+
+CREATE history_detail (
+detail_id INT AUTO_INCREMENT,
+hisotry_id INT,
+item_id INT,
+amount INT,
+subtotal INT,
+created DATETIME,
+updated DATETIME,
+primary key(detail_id)
+);
+
+
+-- finish.phpにて購入が完了した時トランザクション処理を入れる
+-- １．historyテーブルでどのユーザがいつ、いくら購入したかを保存、オートインクリメントでhistory_idを注文番号として作成
+-- ２．history_idを受け取って、history_detailテーブルでどの商品を何個購入したのかを保存
